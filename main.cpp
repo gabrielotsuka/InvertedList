c8c23860bbfd7c9669d57611700342c767a79d40
pedrohba1 <pedro.almeida@ufu.br>
2021-05-14 15:09:27 -0300

    feat: apliquei estilização, mudei linhas 61 e 78

--- main.cpp
+++ main.cpp
@@ -12,55 +12,63 @@
 using namespace std;
 
 // remove pontuacao de uma palavra
-void removePontuacao (char *palavra) {
+void removePontuacao(char *palavra)
+{
     int length = strlen(palavra);
     if (
-        (palavra[length-1] == '.') || (palavra[length-1] == ',') || (palavra[length-1] == ';') ||
-        (palavra[length-1] == ':') || (palavra[length-1] == '?') || (palavra[length-1] == '!')
-       )
-        palavra[length-1] = '\0';
+        (palavra[length - 1] == '.') || (palavra[length - 1] == ',') || (palavra[length - 1] == ';') ||
+        (palavra[length - 1] == ':') || (palavra[length - 1] == '?') || (palavra[length - 1] == '!'))
+        palavra[length - 1] = '\0';
 }
 
 // imprime linha do arquivo com base no offset da palavra
-void imprimeLinha(int offset,FILE *f) {
+void imprimeLinha(int offset, FILE *f)
+{
     int pos = ftell(f);
     char linha[2048];
-    while (pos < offset) {
-        fgets(linha,2047,f);
+    while (pos < offset)
+    {
+        fgets(linha, 2047, f);
         pos = ftell(f);
     }
-    printf("%s",linha);
+    printf("%s", linha);
 }
 
-struct registro {
+struct registro
+{
     int valor;
     int proximo;
 };
 
 // classe que implementa a lista invertida
-class listaInvertida {
+class listaInvertida
+{
 public:
-    map < string, int > secondaryIndexes;
-    FILE* indexFile;
+    map<string, int> secondaryIndexes;
+    FILE *indexFile;
     // construtor
-    listaInvertida() { 
+    listaInvertida()
+    {
         secondaryIndexes.clear();
         indexFile = fopen("indexes.idx", "wb+");
-        if(indexFile == NULL){
+        if (indexFile == NULL)
+        {
             printf("\n\n Nao consegui abrir arquivo de indices. Sinto muito.\n\n\n\n");
-            return ;
+            return;
         }
     }
     // destrutor
-    ~listaInvertida() { 
+    ~listaInvertida()
+    {
         fclose(indexFile);
     }
 
     // adiciona palavra na estrutura
-    void adiciona(char *palavra, int offset) {
-        string aux;
-        for(int i=0; i<strlen(palavra); i++) aux.push_back(palavra[i]);
-        if (!secondaryIndexes.count(aux)) secondaryIndexes[aux] = -1;
+    void adiciona(char *palavra, int offset)
+    {
+        string aux(palavra);
+        if (!secondaryIndexes.count(aux))
+            secondaryIndexes[aux] = -1;
         int offsetStart = secondaryIndexes[aux];
 
         fseek(indexFile, 0, SEEK_END);
@@ -74,55 +82,63 @@ public:
     }
 
     // realiza busca, retornando vetor de offsets que referenciam a palavra
-    int * busca(char *palavra, int *quantidade) {
-        string aux;
-        for(int i=0; i<strlen(palavra); i++) aux.push_back(palavra[i]);
-        if (!secondaryIndexes.count(aux)) secondaryIndexes[aux] = -1;
+    int *busca(char *palavra, int *quantidade)
+    {
+        string aux(palavra);
+        if (!secondaryIndexes.count(aux))
+            secondaryIndexes[aux] = -1;
         int offsetStart = secondaryIndexes[aux];
 
         *quantidade = 0;
-        while (offsetStart != -1) {
+        while (offsetStart != -1)
+        {
             fseek(indexFile, offsetStart, SEEK_SET);
             struct registro reg;
             fread(&reg, sizeof(struct registro), 1, indexFile);
-            
-            *quantidade += 1;   
+
+            *quantidade += 1;
 
             offsetStart = reg.proximo;
         }
 
         offsetStart = secondaryIndexes[aux];
 
-        int* offsets = new int[*quantidade];
+        int *offsets = new int[*quantidade];
         int cnt = 0;
-        while (offsetStart != -1) {
+        while (offsetStart != -1)
+        {
             fseek(indexFile, offsetStart, SEEK_SET);
             struct registro reg;
             fread(&reg, sizeof(struct registro), 1, indexFile);
-            
+
             offsets[cnt++] = reg.valor;
 
             offsetStart = reg.proximo;
         }
         return offsets;
     }
+
 private:
 };
 
 // programa principal
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     // abrir arquivo
     ifstream in("biblia.txt");
-    if (!in.is_open()){
+    if (!in.is_open())
+    {
         printf("\n\n Nao consegui abrir arquivo biblia.txt. Sinto muito.\n\n\n\n");
     }
-    else{
+    else
+    {
         // vamos ler o arquivo e criar a lista invertida com as palavras do arquivo
         char *palavra = new char[100];
         int offset, contadorDePalavras = 0;
         listaInvertida lista;
         // ler palavras
-        while (!in.eof()) {
+        while (!in.eof())
+        {
             // ler palavra
             in >> palavra;
             // pegar offset
@@ -130,39 +146,46 @@ int main(int argc, char** argv) {
             // remover pontuacao
             removePontuacao(palavra);
             // desconsiderar palavras que sao marcadores do arquivo
-            if (!((palavra[0] == '#') || (palavra[0] == '[') || ((palavra[0] >= '0') && (palavra[0] <= '9')))) {
+            if (!((palavra[0] == '#') || (palavra[0] == '[') || ((palavra[0] >= '0') && (palavra[0] <= '9'))))
+            {
                 //printf("%d %s\n", offset,palavra); fflush(stdout); // debug :-)
                 lista.adiciona(palavra, offset);
                 contadorDePalavras++;
-                if (contadorDePalavras % 1000 == 0) { printf(".");  fflush(stdout); }
+                if (contadorDePalavras % 1000 == 0)
+                {
+                    printf(".");
+                    fflush(stdout);
+                }
             }
         }
         in.close();
 
         // agora que ja construimos o indice, podemos realizar buscas
-        do {
+        do
+        {
             printf("\nDigite a palavra desejada ou \"SAIR\" para sair: ");
-            scanf("%s",palavra);
-            if (strcmp(palavra,"SAIR") != 0) {
+            scanf("%s", palavra);
+            if (strcmp(palavra, "SAIR") != 0)
+            {
                 int quantidade;
                 // busca na lista invertida
-                int *offsets = lista.busca(palavra,&quantidade);
+                int *offsets = lista.busca(palavra, &quantidade);
                 // com vetor de offsets, recuperar as linhas que contem a palavra desejada
                 printf("quantidade -> %d\n", quantidade);
-                if (quantidade > 0) {
-                    FILE *f = fopen("biblia.txt","rt");
-                    for (int i = quantidade-1; i >= 0; i--)
-                        imprimeLinha(offsets[i],f);
+                if (quantidade > 0)
+                {
+                    FILE *f = fopen("biblia.txt", "rt");
+                    for (int i = quantidade - 1; i >= 0; i--)
+                        imprimeLinha(offsets[i], f);
                     fclose(f);
                 }
                 else
-                    printf("nao encontrou %s\n",palavra);
+                    printf("nao encontrou %s\n", palavra);
             }
-        } while (strcmp(palavra,"SAIR") != 0);
+        } while (strcmp(palavra, "SAIR") != 0);
 
         printf("\n\nAte mais!\n\n");
     }
 
     return (EXIT_SUCCESS);
 }
-
